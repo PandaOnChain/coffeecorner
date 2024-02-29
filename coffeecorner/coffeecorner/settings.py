adeb4b2c91dea2b7a521d93a8cb3567d264e74ff
ca6ypo <ca6ypo@gmail.com>
2024-02-29 13:12:22 +0100

    minor fix root_static

--- coffeecorner/coffeecorner/settings.py
+++ coffeecorner/coffeecorner/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "cart.context_processors.cart"
+                "cart.context_processors.cart",
             ],
         },
     },
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
