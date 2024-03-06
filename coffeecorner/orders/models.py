72e9cf7bfba2fdedf49801a84cb85f9c269d5e65
ca6ypo <ca6ypo@gmail.com>
2024-03-06 13:25:23 +0100

    coupon apply to cart also was in previous commit,  add coupon to order

--- coffeecorner/orders/models.py
+++ coffeecorner/orders/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from coupons.models import Coupon
 from shop.models import Product
+from django.core.validators import MinValueValidator, MaxValueValidator
+from decimal import Decimal
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -11,26 +14,40 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    coupon = models.ForeignKey(Coupon, related_name="orders", null=True, blank=True, on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+
+    def get_total_cost_before_discount(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        
 
     class Meta:
-        ordering = ['-created']
-        indexes = [models.Index(fields=['-created'])]
+        ordering = ["-created"]
+        indexes = [models.Index(fields=["-created"])]
 
     def __str__(self):
-        return f'Order {self.id}'
-    
+        return f"Order {self.id}"
+
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
+
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, related_name="order_items", on_delete=models.CASCADE
+    )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
-    
+
     def __str__(self):
         return str(self.id)
-    
+
     def get_cost(self):
-        return self.price*self.quantity
-    
+        return self.price * self.quantity
